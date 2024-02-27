8499f4f40de57404e511f01c0167cb4ee29d0ab6
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2024-02-27 17:37:09 +0500

    Урок 12:разделение на компоненты

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+   <div>
+    ava+description
+   </div>
+   <div>
+    My posts
+    <div>
+      New post
+    </div>
+    <div>
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+   </div>
+</div>
+}
+export default Profile;
