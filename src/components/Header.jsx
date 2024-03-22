04ad7747d1b78e02fa3320664c766088e0c54c6a
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2024-03-22 13:02:28 +0500

    Урок 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
     <img src='https://images.squarespace-cdn.com/content/v1/5acd005a8f51302409dafb64/1674234091551-A59YK4R5S81NLNLD6CQ9/Mars-Logo.png'></img>
    </header>;
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
     <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
    <div>
     ava+description
@@ -11,11 +12,13 @@ const Profile = () => {
     <div>
       New post
     </div>
-    <div>
+    <div className={s.posts}>
+    <div className={s.item}>
       post 1
     </div>
-    <div>
+    <div className={s.item}>
       post 2
+    </div>
     </div>
    </div>
 </div>
